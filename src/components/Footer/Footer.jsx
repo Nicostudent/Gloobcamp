311e7e01dc4eb36d091e39d5bd8bb3af86b1464b
Orbitadoo <juanpaavaldezz@gmail.com>
2024-08-10 16:42:45 -0300

    [ADDED/Footer] SRP a little bit.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,41 +1,26 @@
 import Link from "next/link";
 import SocialMedia from "@/components/Footer/SocialMedia";
+import NavigationLink from "@/components/Footer/NavigationLink";
+import Container from "@/components/Footer/Container";
 const Footer = () => {
   return (
     <footer className="gap-2 grid mx-auto my-12 px-6 max-w-7xl">
-      <div className="flex sm:flex sm:flex-row flex-col justify-center sm:justify-between items-center gap-2">
-        <div className="flex flex-wrap gap-8">
-          <Link
-            href="/"
-            className="font-bold text-md text-secondary md:text-xl hover:text-primary duration-300 ease-in-out"
-          >
-            Home
-          </Link>
-          <Link
-            href="/aboutus"
-            className="font-bold text-md text-secondary md:text-xl hover:text-primary duration-300 ease-in-out"
-          >
-            About Us
-          </Link>
-          <Link
-            href="/contact-us"
-            className="font-bold text-md text-secondary md:text-xl hover:text-primary duration-300 ease-in-out"
-          >
-            Contact
-          </Link>
-        </div>
+      <Container>
+        <nav className="flex flex-wrap gap-8">
+          <NavigationLink href="/">Home</NavigationLink>
+          <NavigationLink href="/aboutus">About us</NavigationLink>
+          <NavigationLink href="/contact-us">Contact us</NavigationLink>
+        </nav>
 
-        <div className="flex gap-6">
-          <SocialMedia
-            href="https://github.com/Nicostudent/gloobcamp"
-            src="/github-black.svg"
-            alt="Github"
-            size={24}
-          />
-        </div>
-      </div>
+        <SocialMedia
+          href="https://github.com/Nicostudent/gloobcamp"
+          src="/github-black.svg"
+          alt="Github"
+          size={24}
+        />
+      </Container>
 
-      <div className="flex sm:flex sm:flex-row flex-col justify-center sm:justify-between items-center gap-4">
+      <Container>
         <div className="flex flex-col items-center sm:items-start gap-2">
           <p className="font-bold text-md text-tertiary">
             Do you want to work with us?
@@ -51,7 +36,7 @@ const Footer = () => {
           <p className="text-sm text-tertiary">© 2024. All rights reserved.</p>
           <p className="text-sm text-tertiary">Made with ❤️ in Argentina.</p>
         </div>
-      </div>
+      </Container>
     </footer>
   );
 };
