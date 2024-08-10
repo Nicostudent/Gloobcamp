386d5d2ee5775e93bc54b6038b8ef7655d134c2b
Nicostudent <rojo.nico@hotmail.com>
2024-08-10 11:08:46 -0300

    EndTopic component update, text added and some styles, props removed

--- src/components/EndTopic.jsx
+++ src/components/EndTopic.jsx
@@ -1,12 +1,11 @@
 import Link from "next/link";
 import Paragraph from "./Writing/Paragraph";
-const EndTopic = ({paragraph = null}) => {
+import { LinkButton } from "./LinkButton";
+const EndTopic = () => {
   return (
-    <div>
-      <div>
-        <Paragraph>{paragraph}</Paragraph>
-        <Link href="/" aria-label="back to homepage" >Volver</Link>
-      </div>
+    <div className="flex flex-col gap-10 justify-center items-center  absolute top-0 bottom-10 left-0 right-0 m-auto">      
+        <Paragraph> Congratulations, you have completed this topic</Paragraph>
+        <LinkButton  dark={true} href="/content" label={"Volver"} aria-label="back to homepage"/>
     </div>
   );
 };
