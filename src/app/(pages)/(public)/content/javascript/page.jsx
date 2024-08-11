9579e9ee4d52f2360c125ca614fdc6a8df76379b
Jonathan Arriazu <joni.arriazu2@gmail.com>
2024-08-11 20:02:00 -0300

    feat: search bar added

--- src/app/(pages)/(public)/content/javascript/page.jsx
+++ src/app/(pages)/(public)/content/javascript/page.jsx
@@ -1,7 +1,6 @@
 "use client";
 import Heading from "@/components/Writing/Heading";
 import SubTitle from "@/components/Writing/SubTitle";
-
 import jsTopics from "@/utils/js-topics-and-questions.json";
 import Card from "@/components/Dashboard/Card";
 
@@ -9,10 +8,13 @@ import ProgressBar from "@/components/ProgressBar";
 import useProgress from "@/hooks/useProgress";
 import CompletedContent from "@/components/Dashboard/CompletedContent";
 import { useState } from "react";
-import Input from "@/components/Input";
+import SearchModal from "@/components/SearchBar";
+import CustomButton from "@/components/CustomButton";
+import { FaSearch } from "react-icons/fa";
 
 const Subject = () => {
   const [searchTopic, setSearchTopic] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const { progress, selectedTopics, handleCheckboxChange } = useProgress(
     jsTopics.length
@@ -29,29 +31,36 @@ const Subject = () => {
     topic.title.toLocaleLowerCase().includes(searchTopic.toLocaleLowerCase())
   );
 
+  const handleInputClick = () => {
+    setIsModalOpen(true);
+  };
+  
+
   return (
-    <section className="space-y-4 py-10">
+    <section className="flex flex-col gap-4 py-10">
       <Heading>JavaScript</Heading>
       <SubTitle>Content</SubTitle>
-      <Input
-        className="lg-w-1/3 md:w-1/4"
-        type="text"
-        value={searchTopic}
-        onChange={(e) => setSearchTopic(e.target.value)}
-        placeholder="Search..."
+      <CustomButton
+        purple
+        onClick={handleInputClick}
+        className="gap-4 p-4 w-44 text-white"
+        text="Search..."
+        icon={<FaSearch />} 
+      /> 
+      <SearchModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        searchValue={searchTopic}
+        setSearchValue={setSearchTopic}
+        results={filteredTopics}
+        topicPath="/content/javascript"
       />
-      {filteredTopics.length === 0 ? (
-        <p className="text-center">Not page found</p>
-      ) : (
-        <>
-          <ProgressBar progress={progress} />
-          <Card
-            handleCheckboxChange={handleCheckboxChange}
-            topics={filteredTopics}
-            selectedTopics={selectedTopics}
-          />
-        </>
-      )}
+      <ProgressBar progress={progress} />
+      <Card
+        handleCheckboxChange={handleCheckboxChange}
+        topics={jsTopics}
+        selectedTopics={selectedTopics}
+      />
     </section>
   );
 };
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useRef } from "react";
+import Link from "next/link";
+import Input from "./Input";
+import HighlightText from "@/helpers/HighlightText";
+
+const SearchModal = ({ isOpen, onClose, searchValue, setSearchValue, results, topicPath }) => {
+  const inputRef = useRef(null);
+
+  const handleClose = () => {
+    setSearchValue("");
+    onClose();
+  };
+  
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === "Escape") {
+        handleClose();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener("keydown", handleKeyDown);
+      inputRef.current?.focus();
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [isOpen, onClose]);
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="z-50 fixed inset-0 flex justify-center items-center bg-black bg-opacity-50">
+      <div className="relative bg-primary shadow-lg mx-4 md:mx-4 rounded-lg w-full max-w-lg">
+        <div className="relative border-gray-700 p-4 border-b">
+          <div className="flex items-center gap-4">
+            <Input
+              type="text"
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+              placeholder="Search"
+              className="flex bg-white p-2 border rounded w-full text-black focus:outline-none"
+            />
+            <button 
+              className="flex items-center bg-indigo-300 px-2 rounded h-5"
+              onClick={handleClose} type="button"
+            >
+              x
+            </button>
+          </div>
+          {searchValue ? 
+            <div className="left-0 absolute bg-white shadow-lg mt-2 rounded w-full max-h-64 text-primary overflow-y-auto">
+              {results.length > 0 ? (
+                results.map((result, index) => (
+                  <Link
+                    key={index}
+                    href={`${topicPath}/${result.path}`}
+                    className="flex items-center hover:bg-indigo-100 p-2 text-black cursor-pointer"
+                  >
+                    <HighlightText text={result.title} searchValue={searchValue} />
+                  </Link>
+                ))
+              ) : (
+                <p className="p-2 text-center">No results found for "{searchValue}"</p>
+              )}
+            </div>
+            :
+            <div className="left-0 absolute bg-white shadow-lg mt-2 rounded w-full max-h-64 text-primary overflow-y-auto">
+              <p className="p-2 text-center">Search something</p>
+            </div>
+          }
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchModal;
