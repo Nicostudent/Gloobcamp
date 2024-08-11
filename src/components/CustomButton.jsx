c330b2f983d808a4df6d28511ef4bcba5fd524a0
Jonathan Arriazu <joni.arriazu2@gmail.com>
2024-08-11 20:01:26 -0300

    fix: CustomButton modified and changes applied

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,5 +1,5 @@
-const CustomButton = ({ children, purple, className, text, disabled, ...props }) => {
-  const baseStyles = "w-full h-12 text-center text-base font-semibold leading-6 rounded-md";
+const CustomButton = ({ children, purple, className, text, disabled, icon, ...props }) => {
+  const baseStyles = "w-full h-12 text-center text-base font-semibold leading-6 rounded-md flex items-center";
   const purpleStyles = "text-white hover:opacity-90 bg-primary duration-300";
   const defaultStyles = "text-black bg-white hover:bg-gray-50";
   const disabledStyles = "text-white bg-gray-500 cursor-not-allowed";
@@ -8,6 +8,7 @@ const CustomButton = ({ children, purple, className, text, disabled, ...props })
 
   return (
     <button className={`${baseStyles} ${buttonStyles} ${className}`} disabled={disabled} {...props}>
+      {icon && <span className="mr-2">{icon}</span>}
       {text}
     </button>
   );
