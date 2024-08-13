aaf48c14d7e00f307f791f7ff91506de904627c4
Jonathan Arriazu <joni.arriazu2@gmail.com>
2024-08-12 21:50:45 -0300

    fix: Input component syntax modified

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
-const Input = React.forwardRef(({ type, placeholder, className, ...props }, ref) => (
-  <input
+const Input = React.forwardRef(function Input({ type, placeholder, className, ...props }, ref) {
+  return (
+    <input
       ref={ref}
       type={type}
       className={`w-full shadow-sm text-gray-600 placeholder-text-400 text-lg font-normal leading-7 rounded-xl border border-gray-200 focus:outline-none py-2 px-4 ${className}`}
       placeholder={placeholder}
       {...props}
     />
-));
+  );
+});
 
 export default Input;
