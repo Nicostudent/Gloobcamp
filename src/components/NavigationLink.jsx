cfbe13fde72ada293425d40edd3fe95252925d96
Orbitadoo <juanpaavaldezz@gmail.com>
2024-08-14 10:52:13 -0300

    [UPDATED/NavBar] New Interface && [EDITED/Writing] Now, the Writing components are in one component with export function

--- src/components/NavigationLink.jsx
+++ src/components/NavigationLink.jsx
@@ -3,9 +3,12 @@ const NavigationLink = ({ children, href, size = null, bgIsdark = null }) => {
   return (
     <Link
       href={href}
-      className={`font-bold ${size || ("text-md" && "md:text-lg")} ${
-        bgIsdark ? "text-white" : "text-secondary"
-      } hover:text-primary duration-300 ease-in-out`}
+      className={`font-semibold ${size || "text-sm md:text-base"} ${
+        bgIsdark
+          ? "text-white hover:text-secondary"
+          : "text-secondary hover:text-primary"
+      } duration-300 ease-in-out`}
+      aria-label={`${children} navigation link`}
     >
       {children}
     </Link>
