0c758787593379ce1ae354ff263c5b94c958450b
Orbitadoo <juanpaavaldezz@gmail.com>
2024-08-14 09:24:14 -0300

    [ADDED/NavigationLink] bgIsDark prop

--- src/components/NavigationLink.jsx
+++ src/components/NavigationLink.jsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
-const NavigationLink = ({ children, href, size = null }) => {
+const NavigationLink = ({ children, href, size = null, bgIsdark = null }) => {
   return (
     <Link
       href={href}
-      className={`font-bold ${
-        size || ("text-md" && "md:text-lg")
-      } text-secondary hover:text-primary duration-300 ease-in-out`}
+      className={`font-bold ${size || ("text-md" && "md:text-lg")} ${
+        bgIsdark ? "text-white" : "text-secondary"
+      } hover:text-primary duration-300 ease-in-out`}
     >
       {children}
     </Link>
