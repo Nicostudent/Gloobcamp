d59ee574298c0321c5b257b6ae5da608b5990f29
Emanuel <emanuelrojasguindan@gmail.com>
2024-08-15 17:26:42 -0300

    dinamic rendering logic on sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,21 +1,33 @@
 import jsTopics from "@/utils/js-topics-and-questions.json";
-import Link from "next/link";
+import reactTopics from "@/utils/react-topics-and-questions.json";
 import { useState } from "react";
+import TopicList from "./TopicList";
 
 const Sidebar = ({ pathname }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleSidebar = () => setIsOpen(!isOpen);
   const closeSidebar = () => setIsOpen(false);
+
+  let topics = [];
+  let title = "";
+
+  if (pathname.startsWith("/content/javascript")) {
+    topics = jsTopics.map(topic => ({ ...topic, basePath: "/content/javascript" }));
+    title = "JavaScript";
+  } else if (pathname.startsWith("/content/react")) {
+    topics = reactTopics.map(topic => ({ ...topic, basePath: "/content/react" }));
+    title = "React";
+  }
   return (
     <>
       <button
-        className="md:hidden fixed top-4 left-4 z-20 bg-gray-800 text-white p-2 rounded-md"
+        className="top-4 left-4 z-20 fixed md:hidden bg-gray-800 p-2 rounded-md text-white"
         onClick={toggleSidebar}
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
+          className="w-6 h-6"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
@@ -29,30 +41,13 @@ const Sidebar = ({ pathname }) => {
         </svg>
       </button>
 
-      {/* Overlay para cerrar el sidebar cuando está abierto en móviles */}
-      {isOpen && <div className="fixed inset-0 bg-black opacity-50 z-10" onClick={closeSidebar}></div>}
+      {isOpen && <div className="z-10 fixed inset-0 bg-black opacity-50" onClick={closeSidebar}></div>}
 
-      <aside className="w-64 top-16 overflow-y-scroll fixed bg-gray-200" style={{ height: "93%" }}>
-        <nav className="flex flex-col p-4">
-          <h2 className="text-2xl font-bold text-gray-900 mb-4">Javascript</h2>
-          <ul className="space-y-2">
-            {jsTopics.map((topic) => {
-              const topicPath = `/content/javascript${topic.path}`;
-              const isActive = pathname === topicPath;
-              return (
-                <li key={topic.id} className={`rounded-md ${isActive ? 'bg-green-500' : 'hover:bg-gray-300'}`}>
-                  <Link
-                    aria-label={`go to ${topic.path}`}
-                    href={`/content/javascript/${topic.path}`}
-                    className={`block p-2 text-sm ${isActive ? 'text-white' : 'text-gray-800'}`}
-                  >
-                    {topic.title}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
+      <aside className={`top-16 fixed bg-gray-200 w-64 overflow-y-scroll z-20 transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out md:translate-x-0`} style={{ height: "93%" }}>
+       <nav className="flex flex-col p-4">
+        <h2 className="mb-4 font-bold text-2xl text-gray-900">{title}</h2>
+        <TopicList topics={topics} pathname={pathname} />
+       </nav>
       </aside>
     </>
   );
