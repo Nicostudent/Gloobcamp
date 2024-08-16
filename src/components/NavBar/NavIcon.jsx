dcbc2deb37704d26e1ba3bd8e6b76017e2b84860
Orbitadoo <juanpaavaldezz@gmail.com>
2024-08-16 12:08:34 -0300

    [FIXED/Navbar] The Mobile Open-Nav Icon now is bigger.

--- src/components/NavBar/NavIcon.jsx
+++ src/components/NavBar/NavIcon.jsx
@@ -1,9 +1,17 @@
 import Link from "next/link";
 import Image from "next/image";
 const NavIcon = ({ pathname, image }) => {
+  console.log(Array(image).slice(1, 1));
+
   return (
     <Link href={`${pathname}`} className="lg:block hidden">
-      <Image src={`${image}`} alt={`${image} icon`} width={24} height={24} />
+      <Image
+        src={`${image}`}
+        alt={`${Array(image)} icon`}
+        aria-label={`${image} icon`}
+        width={24}
+        height={24}
+      />
     </Link>
   );
 };
--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
         <div className="md:hidden">
           <BurgerButton isOpen={isOpen} toggleDropdown={toggleDropdown} />
         </div>
-        <div className="flex items-center gap-6 align-middle">
+        <div className="lg:flex items-center gap-6 hidden align-middle">
           <NavIcon pathname={"/"} image={"/user.svg"} />
         </div>
       </div>
