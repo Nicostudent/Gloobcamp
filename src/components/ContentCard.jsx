ec32eca1dff235c5d0be6ca461a076527ba76d5f
Leonardo Juan Pablo Valdez <105684685+Orbitado@users.noreply.github.com>
2024-08-16 13:32:33 -0300

    [ADDED/ContentCard] Initial Structure  (#52)
    * sidebar and footer changes
    
    * Update Styles
    
    * sidebar styles
    
    * sidebar layout implementation
    
    * [ADDED/ContentCard] Created the initial structure of the cards
    
    * [ADDING/ContentCard] Completed
    
    * [ADDED/ContentCard] Color Primary and removed the BG-WHITE
    
    ---------
    
    Co-authored-by: Emanuel <emanuelrojasguindan@gmail.com>
    Co-authored-by: Nicostudent <rojo.nico@hotmail.com>

---
+++ src/components/ContentCard.jsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import { LinkButton } from "./LinkButton";
+
+const ContentCard = ({ children, image, subject }) => {
+  const pathname = String(subject).toLowerCase();
+  return (
+    <article className="flex flex-col flex-wrap shadow-xl p-8 rounded-lg w-full max-w-xs">
+      <div className="flex flex-row gap-4 mb-5">
+        <Image
+          src={image}
+          alt="image1"
+          className="aspect-square"
+          width={50}
+          height={50}
+        />
+        <div>
+          <p className="text-tertiary">TUTORIAL</p>
+          <h3 className="font-bold text-2xl">
+            {subject}
+            <span className="text-primary">.</span>
+          </h3>
+        </div>
+      </div>
+      <div className="h-44 text-tertiary overflow-hidden">{children}</div>
+      <div className="flex justify-end mt-8">
+        <LinkButton
+          dark
+          href={`/content/${pathname}`}
+          label={`Learn ${subject}`}
+        />
+      </div>
+    </article>
+  );
+};
+
+export default ContentCard;
