5b35e759f57c4e035cfa208eb456cc6a35b1fea3
Nicostudent <rojo.nico@hotmail.com>
2024-08-17 11:21:23 -0300

    images move into a new folder, fix the errors in the import. NavigationLink new prop added, fix the error in the toggle

--- src/components/NavigationLink.jsx
+++ src/components/NavigationLink.jsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
-const NavigationLink = ({ children, className, href, size = null}) => {
+const NavigationLink = ({ children, className, href, size = null, onClick = null }) => {
   return (
     <Link
+      onClick={onClick}
       href={href}
       className={`font-semibold ${size || "text-sm md:text-base"} my-2 hover:scale-105 duration-300 ease-in-out ${className}`}
       aria-label={`${children} navigation link`}
