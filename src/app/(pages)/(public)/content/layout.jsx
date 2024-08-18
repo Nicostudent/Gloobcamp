4558d9642378c1258db24ae864e94d25c92f5a83
Emanuel <emanuelrojasguindan@gmail.com>
2024-08-18 20:52:13 -0300

    [PULL from main to sidebar]

--- src/app/(pages)/(public)/content/layout.jsx
+++ src/app/(pages)/(public)/content/layout.jsx
@@ -4,15 +4,9 @@ export default function Layout({ children }) {
   return (
     <>
       <Breadcrumb
-<<<<<<< HEAD
-        homeElement={'Home'}
-        separator={<span> | </span>}
-        capitalizeLinks
-=======
           homeElement={'Home'}
           separator={<span className="dark:text-white"> | </span>}
           capitalizeLinks
->>>>>>> 3b58ed6bb5dce6886572368028dd9d5da51bb9cc
       />
       <main>{children}</main>
     </>
