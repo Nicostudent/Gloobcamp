066f3864cf2d286b403366346bd138daf88646b2
Carmelo C <carmelo2055@gmail.com>
2024-08-19 18:21:04 -0300

    [ADD] localStorage to store the rewards state

--- src/components/NavBar/DailyRewardsList.jsx
+++ src/components/NavBar/DailyRewardsList.jsx
@@ -5,13 +5,39 @@ const DailyRewardsList = () => {
   const [isListOpen, setIsListOpen] = useState(false);
   const listRef = useRef(null);
 
+  // Initial rewards state with points and claimed status
+  const initialRewards = [
+    { label: "Complete 1 subtopic", points: 10, claimed: false },
+    { label: "Complete 3 subtopics", points: 30, claimed: false },
+    { label: "Complete 10 subtopics", points: 60, claimed: false },
+    { label: "Complete a full topic", points: 100, claimed: false },
+  ];
+
+  const [rewards, setRewards] = useState(initialRewards);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const storedRewards = localStorage.getItem("rewards");
+      if (storedRewards) {
+        setRewards(JSON.parse(storedRewards));
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      localStorage.setItem("rewards", JSON.stringify(rewards));
+//      console.log("Rewards state updated succesfully ", rewards)
+    }
+  }, [rewards]);
+
   const toggleList = () => {
     setIsListOpen(!isListOpen);
   };
 
   const handleClickOutside = (event) => {
     if (listRef.current && !listRef.current.contains(event.target)) {
-      setIsListOpen(false); 
+      setIsListOpen(false);
     }
   };
 
@@ -26,13 +52,6 @@ const DailyRewardsList = () => {
       document.removeEventListener("click", handleClickOutside);
     };
   }, [isListOpen]);
-
-  const [rewards, setRewards] = useState([
-    { label: "Complete 1 subtopic", points: 10, claimed: false },
-    { label: "Complete 3 subtopics", points: 30, claimed: false },
-    { label: "Complete 10 subtopics", points: 60, claimed: false },
-    { label: "Complete a full topic", points: 100, claimed: false },
-  ]);
 
   const redeemPoints = (index) => {
     if (rewards[index].claimed || rewards[index].points === 0) return;
@@ -49,7 +68,7 @@ const DailyRewardsList = () => {
         }
         return newRewards;
       });
-    }, 100); // Time interval
+    }, 100); // time interval
   };
 
   return (
@@ -61,7 +80,7 @@ const DailyRewardsList = () => {
         className="cursor-pointer w-8 h-8"
       />
       {isListOpen && (
-        <ul className="absolute left-0 mt-6 w-80">
+        <ul className="absolute left-8 mt-12 w-80">
           {rewards.map((reward, index) => (
             <li
               key={index}
