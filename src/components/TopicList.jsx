a9ac410fa5c759e5536fc959e0e6f6ddaae22feb
nico.rojo <70242834+Nicostudent@users.noreply.github.com>
2024-08-29 17:42:17 -0300

    Fetching json (#64)
    * jsons consume difently. now we could change the json for a fetch if we want to
    
    * jsons doble title fixed

--- src/components/TopicList.jsx
+++ src/components/TopicList.jsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 
 const TopicList = ({ topics, pathname }) => {
+  console.log("pathname", pathname)
   return (
     <ul className="space-y-2">
       {topics.map((topic) => {
-        const topicPath = `${topic.basePath}${topic.path}`;
-        const isActive = pathname === topicPath;
+        const topicPath = `/content/${pathname.split("/")[2]}${topic.path}`;
+        const isActive = `/${pathname.split("/")[3]}` === topic.path
 
         return (
           <li key={topic.id} className={`rounded-md ${isActive ? 'bg-primary' : 'hover:bg-gray-300 dark:hover:bg-stone-600'}`}>
