11bea03be1d7566450c62a30ab9f70fb78b53bb3
Nicostudent <rojo.nico@hotmail.com>
2024-09-03 11:25:05 -0300

    Hydratation bug at ContentPage fiexd,CopyCodeButton refactorized, Quiz now show the incorret answer when finish

--- src/components/ContentPage.jsx
+++ src/components/ContentPage.jsx
@@ -5,18 +5,23 @@ import CopyCodeButton from "./CopyCodeButton";
 
 const ContentPage = ({ title, content, children = null }) => {
   const components = {
+    p({ node, children }) {
+      // Render only if the paragraph does not contain a code block.
+      if (node.children.some(child => child.tagName === "code")) {
+        return <>{children}</>; // Render code blocks directly without <p> wrapper
+      }
+      return <p>{children}</p>;
+    },
     code({ node, inline, className, children, ...props }) {
       const codeString = String(children).trim();
-      return (
-        <div className="relative">
+      return inline ? (
+        <code className={className} {...props}>{codeString}</code>
+      ) : (
+        <div className="code-block relative">
           <pre className={className} {...props}>
             <code>{codeString}</code>
           </pre>
-          {!inline && (
-            <div className="absolute top-2 right-2">
-              <CopyCodeButton code={codeString} />
-            </div>
-          )}
+          <CopyCodeButton code={codeString} />
         </div>
       );
     },
@@ -24,9 +29,7 @@ const ContentPage = ({ title, content, children = null }) => {
 
   return (
     <div className="flex flex-col justify-between items-center md:gap-10 my-12 w-full h-full">
-      <h1 className="font-bold text-2xl text-center md:text-4xl dark:invert">
-        {title}
-      </h1>
+      <h1 className="font-bold text-2xl text-center md:text-4xl dark:invert">{title}</h1>
       <div className="flex justify-center items-center w-full">
         <div className="w-full prose md:prose-lg text-left dark:invert markdown-content">
           <ReactMarkdown components={components}>{content}</ReactMarkdown>
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -11,15 +11,11 @@ const decodeHTML = (html) => {
 
 const Quiz = ({ questions, nextTopicPath, topicId, subject }) => {
   const [questionIndex, setQuestionIndex] = useState(0);
-
   const [isFinished, setIsFinished] = useState(false);
-
-  const [correctChoices, setCorrectChoices] = useState(0);
-
-  const [incorrectChoices, setIncorrectChoices] = useState(0);
+  const [correctChoices, setCorrectChoices] = useState([]);
+  const [incorrectChoices, setIncorrectChoices] = useState([]);
 
   const currentQuestion = questions[questionIndex];
-
   const totalQuestions = questions.length;
 
   useEffect(() => {
@@ -34,9 +30,12 @@ const Quiz = ({ questions, nextTopicPath, topicId, subject }) => {
     const selectedChoice = decodeHTML(e.target.innerHTML);
     const correctAnswer = decodeHTML(currentQuestion.correctAnswer);
     if (selectedChoice == correctAnswer) {
-      setCorrectChoices((prev) => prev + 1);
+      setCorrectChoices((prev) => [...prev, selectedChoice]);
     } else {
-      setIncorrectChoices((prev) => prev + 1);
+      setIncorrectChoices((prev) => [
+        ...prev,
+        { choice: selectedChoice, index: questionIndex },
+      ]);
     }
 
     if (questionIndex < totalQuestions) {
@@ -64,34 +63,44 @@ const Quiz = ({ questions, nextTopicPath, topicId, subject }) => {
 
   const handleRestartQuiz = () => {
     setQuestionIndex(0);
-    setCorrectChoices(0);
-    setIncorrectChoices(0);
+    setCorrectChoices([]);
+    setIncorrectChoices([]);
     setIsFinished(false);
   };
-console.log("correct", correctChoices)
-console.log("incorrectChoices", incorrectChoices)
 
-
+  console.log(correctChoices, incorrectChoices);
+  console.log("questionIndex", questionIndex)
   if (isFinished) {
     return (
       <div className="flex flex-col justify-center items-center border-2 bg-primary dark:bg-stone-900 p-6 rounded-xl max-w-7xl h-full text-center text-gray-100">
         <h2 className="mb-6 font-black text-2xl text-center md:text-4xl">
           Quiz Result!
         </h2>
+        <p className="text-xl mb-6">Correct Answers: {correctChoices.length}</p>
         <div className="mb-6">
-          <p className="text-xl">Correct Answers: {correctChoices}</p>
-        </div>
-        <div className="mb-6">
-          <p className="text-xl">Incorrect Answers: {incorrectChoices}</p>
+          <p className="text-xl mb-2">
+            Incorrect Answers: {incorrectChoices.length}
+          </p>
+          <ul>
+            {incorrectChoices.map((item, index) => (
+              <li className="border-2 m-1  px-2 bg-slate-500 rounded-lg opacity-65" key={index}>
+                <p className="text-start">Question {item.index + 1}: {questions[item.index].question}</p>
+                <p className="text-start">Your answer: {item.choice} <span className="text-red-500 drop-shadow-sm font-bold items-center text-center">X</span></p>
+                {/* <p>Correct answer: {questions[item.index].correctAnswer}</p> */}
+              </li>
+            ))}
+          </ul>
         </div>
         <div className="flex md:flex-row flex-col justify-center items-center">
-          {incorrectChoices !== 0 ? (
-            <button
-              className="bg-gray-100 dark:bg-primary hover:opacity-90 md:mx-3 mb-3 md:mb-0 p-3 rounded-md w-fit font-semibold text-center text-primary dark:text-white"
-              onClick={handleRestartQuiz}
-            >
-              Try again
-            </button>
+          {incorrectChoices.length !== 0 ? (
+            <div>
+              <button
+                className="bg-gray-100 dark:bg-primary hover:opacity-90 md:mx-3 mb-3 md:mb-0 p-3 rounded-md w-fit font-semibold text-center text-primary dark:text-white"
+                onClick={handleRestartQuiz}
+              >
+                Try again
+              </button>
+            </div>
           ) : (
             <Link
               href={nextTopicPath}
